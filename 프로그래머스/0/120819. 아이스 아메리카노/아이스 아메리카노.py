9d7be77fb9a15575836c73b8649dfbcab3bd5060
Seoyeon <113488264+syu357@users.noreply.github.com>
2024-09-11 19:22:25 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -0,0 +1,10 @@
+def solution(money):
+    answer = []
+    
+    cup = money // 5500
+    change = money % 5500
+        
+    answer.append(cup)
+    answer.append(change)
+    
+    return answer
